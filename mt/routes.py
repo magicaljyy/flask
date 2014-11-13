2185c2604d2d8574b7ce034b55343425d6ebfdfc
magicaljyy <magicaljyy@gmail.com>
2014-11-13 00:28:14 -0800

    trying to figure out timeline

--- mt/routes.py
+++ mt/routes.py
@@ -1,7 +1,7 @@
 from mt import app
 from flask import Flask, render_template, request, flash, session, redirect, url_for
 from forms import SignupForm
-from models import db, User
+from models import db, User, Message
 
 @app.route('/')
 def home():
@@ -9,7 +9,6 @@ def home():
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    print('jyy')
     form = SignupForm()
     
     if request.method=='POST':
@@ -20,8 +19,28 @@ def signup():
             db.session.add(new_user)
             db.session.commit()
             session['email'] = new_user.email
-            return "[1] Create a new user [2] sign in the user [3] redirect to the user's profile"
+            return redirect(url_for('%s'.format(new_user.user_id)))
             
             
     elif request.method == 'GET':
-        return render_template('signup.html', form=form)
+        return render_template('signup.html', form = form)
+
+@app.route('/signin')
+def signin():
+    print('jyy')
+    about(404)
+
+@app.route('/<uid>')
+def user_timeline(uid):
+    if 'email' not in session:
+        return redirect(url_for('signin'))
+    user = User.query.filter_by(user_id = uid).first()
+    if user is None:
+        return redirect(url_for('signin'))
+    else:
+        return render_template('timeline.html',
+            messages = Message.query.join(User, User.user_id == Message.author_id, User.user_id == uid).all(),
+            profile_user = user
+        )       
+
+    
